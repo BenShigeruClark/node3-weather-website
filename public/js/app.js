5d9ff54e895179b831718f4d87f82ceecf935087
Ben <benclarkphoto@mac.com>
2019-12-02 21:51:57 -0800

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Client side javascript is loaded!')
 // variables that link with our HTML
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
